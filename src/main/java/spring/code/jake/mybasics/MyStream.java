ccce91bfca514aa8c3c63f8910cce22405966efd
Panda77 <fight4uking@gmail.com>
2024-05-23 19:53:01 -0400

    MyStream Update

--- src/main/java/spring/code/jake/mybasics/MyStream.java
+++ src/main/java/spring/code/jake/mybasics/MyStream.java
@@ -4,13 +4,32 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class MyStream {
-    
+
+    public static void stream() {
+        List<String> myList = new ArrayList<>(
+                Arrays.asList("Jake", "John", "Jill", "Jenny", "Jesse", "Jen"));
+
+        Optional<Integer> length = myList.stream().filter(s -> s.length() > 3).map(s -> s.length()).reduce((a, b) -> a + b);
+
+        if(length.isPresent()) {
+            System.out.println(length.get());
+        }
+    }
+
     public static void forEach() {
-        List<String> myList = new ArrayList<>(Arrays.asList("Jake", "John", "Jill", "Jenny", "Jesse", "Jen", "Jen", "Jen"));
+        List<String> myList = new ArrayList<>(
+                Arrays.asList("Jake", "John", "Jill", "Jenny", "Jesse"));
 
         myList.forEach(s -> s = s + "!"); // Collection自己的forEach方法，不会改变原来的list
+        myList.forEach(s -> {
+            s = s + "!";
+            System.out.println(s);
+        });
         List<String> myList2 = myList.stream().map(s -> s = s + "?").collect(Collectors.toList());
-
+        myList.forEach(s -> {
+            s = s + "#";
+            System.out.println(s);
+        });
         myList2.forEach(System.out::println);
     }
 }
