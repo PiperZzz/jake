7c42eed0bf0d71534d0ea7a71fc9fa854b2669bc
Panda77 <PiperZzz@users.noreply.github.com>
2024-05-23 18:59:41 -0400

    MyStream Init

--- src/main/java/spring/code/jake/mybasics/MyStream.java
+++ src/main/java/spring/code/jake/mybasics/MyStream.java
@@ -1,5 +1,16 @@
 package spring.code.jake.mybasics;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class MyStream {
     
+    public static void forEach() {
+        List<String> myList = new ArrayList<>(Arrays.asList("Jake", "John", "Jill", "Jenny", "Jesse", "Jen", "Jen", "Jen"));
+
+        myList.forEach(s -> s = s + "!"); // Collection自己的forEach方法，不会改变原来的list
+        List<String> myList2 = myList.stream().map(s -> s = s + "?").collect(Collectors.toList());
+
+        myList2.forEach(System.out::println);
+    }
 }
