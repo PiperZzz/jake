41e4adc4a78abc5e54e577ecf69e5d6c12a53aa9
Panda77 <PiperZzz@users.noreply.github.com>
2024-05-29 20:14:00 -0400

    MyProductInterceptor Update

--- src/main/java/spring/code/jake/myproduct/MyProductInterceptor.java
+++ src/main/java/spring/code/jake/myproduct/MyProductInterceptor.java
@@ -2,21 +2,37 @@ package spring.code.jake.myproduct;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
+@SuppressWarnings("null")
 public class MyProductInterceptor implements HandlerInterceptor {
-    
+
     @Override
-    @SuppressWarnings("null")
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
             return MyJwtUtil.isTokenValid(token);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false; // 身份验证失败
         }
-        return false; // 身份验证失败
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        // 只有preHandle返回true时才会执行
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        // 只有preHandle返回true时才会执行
     }
 }
