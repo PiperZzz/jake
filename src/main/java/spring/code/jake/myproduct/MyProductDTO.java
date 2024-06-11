8af20584c9d05d80f3e7541b9e91e3735ada76b9
Panda77 <PiperZzz@users.noreply.github.com>
2024-06-11 16:59:04 -0400

    update

--- src/main/java/spring/code/jake/myproduct/MyProductDTO.java
+++ src/main/java/spring/code/jake/myproduct/MyProductDTO.java
@@ -2,8 +2,9 @@ package spring.code.jake.myproduct;
 
 import java.util.*;
 
+// record 是Java 14的新特性，用于简化POJO类的定义
 public record MyProductDTO(String productName, List<String> tags) {
     public MyProductDTO { // Compact Constructor for record
-        tags = Collections.unmodifiableList(tags);
+        tags = List.copyOf(tags); // 比Collections.unmodifiableList(tags)更好
     }
 }
