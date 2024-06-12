555d4e59b7872573e5ee50de9c18ca646d0be96c
Panda77 <PiperZzz@users.noreply.github.com>
2024-06-12 09:46:48 -0400

    update SequenceGenerator

--- src/main/java/spring/code/jake/myproduct/MyProductEntity.java
+++ src/main/java/spring/code/jake/myproduct/MyProductEntity.java
@@ -15,7 +15,8 @@ import lombok.*;
 @NoArgsConstructor
 public class MyProductEntity implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_seq")
+    @SequenceGenerator(name = "product_id_seq", sequenceName = "product_id_sequence", allocationSize = 1) // sequenceName是PostgreSQL内定义的序列名
     private Long id;
 
     @Column(name = "product_name", nullable = false)
