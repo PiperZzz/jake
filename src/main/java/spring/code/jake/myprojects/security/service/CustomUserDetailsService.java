2fede3aeee072198d4df6a25ef94721f3dbc3f36
Panda77 <PiperZzz@users.noreply.github.com>
2024-07-07 13:07:46 -0400

    update

---
+++ src/main/java/spring/code/jake/myprojects/security/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package spring.code.jake.myprojects.security.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import spring.code.jake.myprojects.security.dao.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username" + username));
+    }
+
+}
