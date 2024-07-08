056f1df26f829079f7144e7cbc52107bc2fdf025
Panda77 <PiperZzz@users.noreply.github.com>
2024-07-08 10:47:00 -0400

    update

--- src/main/java/spring/code/jake/myprojects/auth/config/SecurityConfig.java
+++ src/main/java/spring/code/jake/myprojects/auth/config/SecurityConfig.java
@@ -2,15 +2,21 @@ package spring.code.jake.myprojects.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 import spring.code.jake.myprojects.auth.filter.JwtAuthFilter;
-import spring.code.jake.myprojects.auth.security.CustomAuthenticationProvider;
+import spring.code.jake.myprojects.auth.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -18,18 +24,34 @@ import spring.code.jake.myprojects.auth.security.CustomAuthenticationProvider;
 public class SecurityConfig {
 
     private final JwtAuthFilter JwtAuthFilter;
-    private final CustomAuthenticationProvider CustomAuthenticationProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(csrf -> csrf.disable())
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, DaoAuthenticationProvider authenticationProvider) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(requests -> requests.requestMatchers("/api/v1/**").permitAll().anyRequest().authenticated())
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(CustomAuthenticationProvider)
+                .authenticationProvider(authenticationProvider)
                 .addFilterBefore(JwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return httpSecurity.build();
+        return http.build();
     }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(CustomUserDetailsService userDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
