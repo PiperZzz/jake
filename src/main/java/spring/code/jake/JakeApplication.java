2ceb6aaa3c88c22fa38d36d5ee68802fe95f0b5e
Panda77 <fight4uking@gmail.com>
2024-07-10 20:06:15 -0400

    update

--- src/main/java/spring/code/jake/JakeApplication.java
+++ src/main/java/spring/code/jake/JakeApplication.java
@@ -3,11 +3,15 @@ package spring.code.jake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 import spring.code.jake.mythreading.MyThread;
 
 @SpringBootApplication
 @EnableCaching
+@Profile("${spring.profiles.active}")
+@PropertySource("classpath:application.properties")
 public class JakeApplication {
 
 	public static void main(String[] args) {
