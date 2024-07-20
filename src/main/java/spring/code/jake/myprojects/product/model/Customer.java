841ebd90d07c8f68f0f4044cfb79777654da1cd0
Panda77 <fight4uking@gmail.com>
2024-07-20 10:41:13 -0400

    update

--- src/main/java/spring/code/jake/myprojects/product/model/Customer.java
+++ src/main/java/spring/code/jake/myprojects/product/model/Customer.java
@@ -1,6 +1,5 @@
 package spring.code.jake.myprojects.product.model;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
--- src/main/java/spring/code/jake/myprojects/product/model/Product.java
+++ src/main/java/spring/code/jake/myprojects/product/model/Product.java
@@ -17,7 +17,6 @@ import org.hibernate.annotations.DynamicInsert;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
