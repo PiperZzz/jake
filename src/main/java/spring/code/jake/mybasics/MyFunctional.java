097cec3bc77c0edfbed3e137f4f5f58765d01422
Panda77 <fight4uking@gmail.com>
2024-07-24 22:35:42 -0400

    update

--- src/main/java/spring/code/jake/mybasics/MyFunctional.java
+++ src/main/java/spring/code/jake/mybasics/MyFunctional.java
@@ -6,8 +6,8 @@ public class MyFunctional {
     
     private <T> T getGenericType (MyClass myClass, Function<MyClass, T> function) {
         // 方法定义实现了根据传入的function来获取MyClass中的不同数据类型
-        // Function接口的第一个参数是输入，第二个参数是输出
-        return function.apply(myClass);
+        // Function接口的第一个参数是输入类型，第二个参数是输出类型
+        return function.apply(myClass); // apply方法会take输入类型为参数并return输出类型
     }
 
     public String getMyString(MyClass myClass) {
@@ -15,7 +15,7 @@ public class MyFunctional {
     }
 
     public Integer getMyInteger(MyClass myClass) {
-        return getGenericType(myClass, MyClass::getInteger);
+        return getGenericType(myClass, (mC) -> mC.getInteger());
     }
 
     class MyClass {
