59e5da6f02af6357e6661f75e6af97542b83d05e
Panda77 <PiperZzz@users.noreply.github.com>
2024-07-29 10:58:04 -0400

    update

--- src/main/java/spring/code/jake/myprojects/auth/dto/RegisterRequest.java
+++ src/main/java/spring/code/jake/myprojects/auth/dto/RegisterRequest.java
@@ -2,7 +2,7 @@ package spring.code.jake.myprojects.auth.dto;
 
 import lombok.Value;
 
-@Value
+@Value // record类Java 16以后开始正式支持，不想装逼直接用@Value注解
 public class RegisterRequest {
     private String username;
     private String password;
