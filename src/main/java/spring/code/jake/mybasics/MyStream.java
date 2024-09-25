a166de01c839f8c38ffbd3611e72e7067c97f5e5
Panda77 <PiperZzz@users.noreply.github.com>
2024-09-25 09:13:48 -0400

    Stream API

--- src/main/java/spring/code/jake/mybasics/MyStream.java
+++ src/main/java/spring/code/jake/mybasics/MyStream.java
@@ -12,6 +12,7 @@ public class MyStream {
         List<String> myList = new ArrayList<>(
                 Arrays.asList("Jake", "John", "Jill", "Jenny", "Jesse", "Jen"));
 
+        /* Optional 也是Java 8新特性 在于优雅的处理null返回值 */
         Optional<Integer> length = myList.stream().filter(s -> s.length() > 3).map(s -> s.length()).reduce((a, b) -> a + b);
         /* filter()方法参数是Predicate<T>, Lambda实现其内部test(T t)方法 返回boolean */
         /* map()方法参数是Function<T, R>, Lambda实现其内部apply(T t)方法 返回泛型R */
@@ -19,6 +20,8 @@ public class MyStream {
         /* forEach(),reduce(),collect()都属于Terminal Operations(终端操作) */
         /* Terminal Operations会触发之前Chain Of Intermediate Operations并结束Stream，返回非Stream对象 */
 
+        /* Lazy Evaluation是Stream API想对传统Iterator的最大优势 */
+        /* 只有Terminal Operations才会触Chain Of Intermediate Operations这种特性，不会有中间状态的集合浪费资源*/
         if(length.isPresent()) {
             System.out.println(length.get());
         }
